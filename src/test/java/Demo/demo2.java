93d49308b18b3e67f7a19f9a07590df256725123
Vinod <Vinod@LAPTOP-OK9MM80Q>
2026-03-12 11:19:01 +0530

    Branch 2

--- src/test/java/Demo/demo2.java
+++ src/test/java/Demo/demo2.java
@@ -5,6 +5,7 @@ public class demo2 {
 	public static void main(String[] args) {
 		System.out.println("Practicing pull operation");
 		System.out.println("Branch created");
+		System.out.println("Brach 2");
 
 	}
 
